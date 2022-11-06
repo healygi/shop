07b4e5490d9f81925d68c22b4463fa0814a3afb0
healygi <healygi@tcd.ie>
2022-11-06 15:25:40 +0000

    push before clodse

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    path('', views.checkout, name='checkout'),
+    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
 ]
