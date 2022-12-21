d5e94722991794c4cf72a697ecff8ff9d05edf97
healygi <healygi@tcd.ie>
2022-12-21 14:06:01 +0000

    update keys

--- shop/settings.py
+++ shop/settings.py
@@ -178,7 +178,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    AWS_STORAGE_BUCKET_NAME = 'gymgeeks'
+    AWS_STORAGE_BUCKET_NAME = 'https://gymgeeks.s3.amazonaws.com'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY')
