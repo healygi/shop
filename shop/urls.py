06300b9b44804c13bb571af5b75a4186cfcf302d
healygi <healygi@tcd.ie>
2022-12-21 23:51:17 +0000

    add 404.html page

--- shop/urls.py
+++ shop/urls.py
@@ -16,6 +16,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,6 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
+handler404 = 'shop.views.handler404'
