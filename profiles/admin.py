50420bc545255424d0ad8e20d1d4d3ea7bf6450f
healygi <healygi@tcd.ie>
2023-02-18 16:29:48 +0000

    register UserProfile Model and WishList modal to admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,1 +1,30 @@
+"""imports for admin page"""
+
 from django.contrib import admin
+from .models import UserProfile, WishListItem
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    """Allows admin to manage profiles via the admin panel"""
+    list_display = (
+     'user',
+     'default_phone_number',
+     'default_street_address1',
+     'default_street_address2',
+     'default_county',
+     'default_postcode',
+     'default_country',
+    )
+
+
+class WishListItemAdmin(admin.ModelAdmin):
+    """Allows admin to manage wishlist items via the admin panel"""
+    readonly_fields = (
+        'user',
+        'product',
+    )
+
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(WishListItem, WishListItemAdmin)
